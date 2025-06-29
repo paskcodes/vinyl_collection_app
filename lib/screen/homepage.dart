259ec4085d3b53e122fed4e2b3037893977e4abb
Pasquale Biele <bielepasquale@gmail.com>
2025-06-29 11:25:49 +0200

    creazione homepage

---
+++ lib/screen/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/search'),
+              child: const Text('Cerca su Discogs'),
+            ),
+            ElevatedButton(
+              onPressed: () => Navigator.pushNamed(context, '/collection'),
+              child: const Text('La mia collezione'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

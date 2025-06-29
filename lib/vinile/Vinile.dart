cb50653e6431e521252016a5d5396e77e6820a61
Pasquale Biele <bielepasquale@gmail.com>
2025-06-29 18:15:53 +0200

    creazione connessione database e recfactor classe vinili

--- lib/vinile/Vinile.dart
+++ lib/vinile/Vinile.dart
@@ -1,79 +1,56 @@
 import 'dart:core';
-import 'dart:ui';
-import 'package:flutter/material.dart';
-
-
-import 'package:vinyl_collection_app/vinile/Condizione.dart';
-import 'package:vinyl_collection_app/vinile/Genere.dart';
+import 'package:vinyl_collection_app/vinile/condizione.dart';
+import 'package:vinyl_collection_app/vinile/genere.dart';
 
 class Vinile{
-  String _titolo;
-  String _nomeArtista;
-  int _anno;
-  Genere _genere;
-  String _etichettaDiscografica;
-  int _quantita;
-  Condizione _condizione;
-  Image _immagine;
-  bool _preferito;
+  final String titolo;
+  final String nomeArtista;
+  final int anno;
+  final Genere genere;
+  final String etichettaDiscografica;
+  final int quantita;
+  final Condizione condizione;
+  final String immagine;
+  final bool preferito;
 
-  Vinile(
-      this._titolo, this._nomeArtista, this._anno,
-      this._genere, this._etichettaDiscografica,
-      this._quantita, this._condizione,
-      String urlImmagine,
-      {bool preferito=false}
-      ) : _immagine = Image.network(urlImmagine),
-          _preferito = preferito;
+  Vinile({
+    required this.titolo,
+    required this.nomeArtista,
+    required this.anno,
+    required this.genere, 
+    required this.etichettaDiscografica,
+    required  this.quantita, 
+    required this.condizione,
+    required this.immagine,
+    this.preferito = false
+    }
+  );
 
-
-  Condizione get condizione => _condizione;
-
-  set condizione(Condizione value) {
-    _condizione = value;
+Map<String, Object?> toMap() {
+    return {
+      'titolo': titolo,
+      'nomeArtista': nomeArtista,
+      'anno': anno,
+      'genere': genere.index, // enum → int
+      'etichettaDiscografica': etichettaDiscografica,
+      'quantita': quantita,
+      'condizione': condizione.index, // enum → int
+      'immagineUrl': immagine,
+      'preferito': preferito ? 1 : 0,
+    };
   }
 
-  int get quantita => _quantita;
-
-  set quantita(int value) {
-    _quantita = value;
+  factory Vinile.fromMap(Map<String, dynamic> map) {
+    return Vinile(
+      titolo: map['titolo'],
+      nomeArtista: map['nome_artista'],
+      anno: map['anno'],
+      genere: Genere.values[map['genere']],
+      etichettaDiscografica: map['etichetta_discografica'],
+      quantita: map['quantita'],
+      condizione: Condizione.values[map['condizione']],
+      immagine: map['immagine'],
+      preferito: map['preferito'] == 1,
+    );
   }
-
-  String get etichetta_discografica => _etichettaDiscografica;
-
-  set etichetta_discografica(String value) {
-    _etichettaDiscografica = value;
-  }
-
-  Genere get genere => _genere;
-
-  set genere(Genere value) {
-    _genere = value;
-  }
-
-  int get anno => _anno;
-
-  set anno(int value) {
-    _anno = value;
-  }
-
-  String get nomeArtista => _nomeArtista;
-
-  set nomeArtista(String value) {
-    _nomeArtista = value;
-  }
-
-  String get titolo => _titolo;
-
-  set titolo(String value) {
-    _titolo = value;
-  }
-
-  Image? get immagine => _immagine;
-
-  set preferito(bool preferito){
-    _preferito=preferito;
-  }
-
-  bool get preferito => _preferito;
 }
